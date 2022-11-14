81fe9371a0d574649293ecbce54e2ffe0d9fa4f6
David Roddy <davidroddy321@gmail.com>
2022-11-14 22:38:23 +0000

    average bkgd subtraction

--- videos.py
+++ videos.py
@@ -8,17 +8,26 @@ PATH = "/mnt/c/users/david/Documents/uni/year-5/ultrasound/"
 
 
 def getFrames():
-    cap = cv2.VideoCapture(PATH + "videos/vid06.mp4")
+    cap = cv2.VideoCapture(PATH + "videos/vid08.mp4")
     total_frames = int(cap.get(7))
 
     widths = []
+    all = np.array([0.0]*586)
+    count = 0
 
     for i in range(1, total_frames):
-        if i % 200 == 0:
+        if i % 50 == 0:
             cap.set(1, i)
             ret, frame = cap.read()
-            width = analyseFrame(frame)
+            width, profile = analyseFrame(frame)
             widths.append(width)
+            all += np.array(profile)
+            count += 1
+
+    all /= count
+    x = np.linspace(0, 600, len(all))
+    plt.plot(x, all)
+    plt.ylim(0, 200)
     
     # x = np.linspace(0, len(widths), len(widths))
 
@@ -29,17 +38,18 @@ def getFrames():
 def analyseFrame(frame):
     # print(frame.shape)
     greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    roi = greyscale[100:700, 690:810]
+    roi = greyscale[114:700, 690:810]   # height, width
     # cv2.imwrite(PATH + "scripts/output.jpg", roi)
-    profile = np.average(list(roi), axis=1)
+    profile = np.average(list(roi), 1)
     x = np.linspace(0, len(profile), len(profile))
-    plt.plot(x, profile)
-    peak, props = find_peaks(profile, prominence=10, width=10)
+    # plt.plot(x, profile)
+    peak, props = find_peaks(profile, prominence=20, width=0)
     width = 0
-    print(len(props['widths']))
-    if len(props['widths']) != 0:
-        width = props['widths'][0]
-    return width
+    # print(f"props: {props}")
+    # print(f"peaks: {peak}\n")
+    # if len(props['widths']) != 0:
+        # width = props['widths'][0]
+    return width, profile
 
 
 getFrames()
