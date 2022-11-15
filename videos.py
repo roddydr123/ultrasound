ef8ff1cd99d9066be2a9c7d8be7d78de939f6e01
David Roddy <davidroddy321@gmail.com>
2022-11-15 15:09:01 +0000

    OOPing it

--- videos.py
+++ videos.py
@@ -6,77 +6,79 @@ from scipy.signal import find_peaks
 
 PATH = "/mnt/c/users/david/Documents/uni/year-5/ultrasound/"
 
+
+class Video():
+    def __init__(self, filename, start_deep):
+        self.filename = filename
+        self.start_deep = start_deep
+        self.cap = cv2.VideoCapture(f"{PATH}/videos/{filename}")
+        self.frame_count = int(self.cap.get(7))
+        self.bkgd = self.get_bkgd()
  
-def get_profile(frame):
-    greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    roi = greyscale[114:700, 690:810]   # height, width
-    profile = np.average(list(roi), 1)
-    # cv2.imwrite(PATH + "scripts/output.jpg", roi)
-    return profile
+    def get_profile(self, frame):
+        greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        roi = greyscale[114:700, 690:810]   # height, width
+        profile = np.average(list(roi), 1)
+        # cv2.imwrite(PATH + "scripts/output.jpg", roi)
+        return profile
+
+    def get_bkgd(self):
+        if self.start_deep is True:
+            self.cap.set(1, self.frame_count - 10)
+            ret, frame = self.cap.read()
+            roi = cv2.selectROI("select image", frame)
+            profile = self.get_profile(frame)
+            end_bkgd = profile[-int(len(profile) / 2):]
+
+            self.cap.set(1, 10)
+            ret, frame = self.cap.read()
+            profile = self.get_profile(frame)
+            start_bkgd = profile[:int(len(profile) / 2)]
+
+            bkgd = np.concatenate((start_bkgd, end_bkgd))
+        else:
+            self.cap.set(1, 10)
+            ret, frame = self.cap.read()
+            profile = self.get_profile(frame)
+            start_bkgd = profile[-int(len(profile) / 2):]
+
+            self.cap.set(1, self.frame_count - 10)
+            ret, frame = self.cap.read()
+            profile = self.get_profile(frame)
+            end_bkgd = profile[:int(len(profile) / 2)]
+
+            bkgd = np.concatenate((start_bkgd, end_bkgd))
+
+        if np.average(bkgd) < 2:
+            bkgd = np.zeros_like(bkgd)
+
+        return bkgd
+
+    def analyseFrame(self, index, subtract_bkgd=True):
+        self.cap.set(1, index)
+        ret, frame = self.cap.read()
+        profile = self.get_profile(frame)
+        if subtract_bkgd is True:
+            profile -= self.bkgd
+        # x = np.linspace(0, len(profile), len(profile))
+        # plt.plot(x, profile)
+        peak, props = find_peaks(profile, distance=len(profile), width=0)
+        width = 0
+        # print(f"props: {props}")
+        # print(f"peaks: {peak}\n")
+        if len(props['widths']) != 0:
+            width = props['widths'][0]
+        return width
 
 
-def get_bkgd(cap, total_frames, start_deep):
-    if start_deep is True:
-        cap.set(1, total_frames - 10)
-        ret, frame = cap.read()
-        profile = get_profile(frame)
-        end_bkgd = profile[-int(len(profile) / 2):]
-
-        cap.set(1, 10)
-        ret, frame = cap.read()
-        profile = get_profile(frame)
-        start_bkgd = profile[:int(len(profile) / 2)]
-
-        bkgd = np.concatenate((start_bkgd, end_bkgd))
-    else:
-        cap.set(1, 10)
-        ret, frame = cap.read()
-        profile = get_profile(frame)
-        start_bkgd = profile[-int(len(profile) / 2):]
-
-        cap.set(1, total_frames - 10)
-        ret, frame = cap.read()
-        profile = get_profile(frame)
-        end_bkgd = profile[:int(len(profile) / 2)]
-
-        bkgd = np.concatenate((start_bkgd, end_bkgd))
-
-    if np.average(bkgd) < 2:
-        bkgd = np.zeros_like(bkgd)
-
-    return bkgd
-
-
-def analyseFrame(frame, bkgd):
-    profile = get_profile(frame)
-    profile -= bkgd
-    x = np.linspace(0, len(profile), len(profile))
-    # plt.plot(x, profile)
-    peak, props = find_peaks(profile, distance=len(profile), width=0)
-    width = 0
-    # print(f"props: {props}")
-    # print(f"peaks: {peak}\n")
-    if len(props['widths']) != 0:
-        width = props['widths'][0]
-    return width
-
-
-def getFrames():
-    start_deep = False
-    cap = cv2.VideoCapture(PATH + "videos/vid04.mp4")
-    total_frames = int(cap.get(7))
-
-    bkgd = get_bkgd(cap, total_frames, start_deep)
-
+def main():
+    vid = Video("vid04.mp4", start_deep=False)
     widths = []
+    width_indices = np.arange(0, vid.frame_count, 50)
+    for index in width_indices:
+        width = vid.analyseFrame(index, subtract_bkgd=True)
+        widths.append(width)
 
-    for i in range(1, total_frames):
-        if i == 0:
-            cap.set(1, i)
-            ret, frame = cap.read()
-            width = analyseFrame(frame, bkgd)
-            widths.append(width)
-    
     x = np.linspace(0, len(widths), len(widths))
     # x = np.linspace(0, len(bkgd), len(bkgd))
 
@@ -84,4 +86,5 @@ def getFrames():
     plt.plot(x, widths)
     plt.show()
 
-getFrames()
+
+main()
