f07b40b4fd00abfcf41b7f73dad40af0e9b9e4b9
David Roddy <davidroddy321@gmail.com>
2022-11-15 12:26:06 +0000

    working simple subtractor

--- videos.py
+++ videos.py
@@ -7,49 +7,63 @@ from scipy.signal import find_peaks
 PATH = "/mnt/c/users/david/Documents/uni/year-5/ultrasound/"
 
 
+def get_profile(frame):
+    greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    roi = greyscale[114:700, 690:810]   # height, width
+    profile = np.average(list(roi), 1)
+    return profile
+
+
+def get_bkgd(cap, total_frames):
+    # cap.set(1, 10)
+    cap.set(1, total_frames - 10)
+    ret, frame = cap.read()
+    profile = get_profile(frame)
+    end_bkgd = profile[-int(len(profile) / 2):]
+
+    # cap.set(1, total_frames - 10)
+    cap.set(1, 10)
+    ret, frame = cap.read()
+    profile = get_profile(frame)
+    start_bkgd = profile[:int(len(profile) / 2)]
+
+    bkgd = np.concatenate((start_bkgd, end_bkgd))
+    return bkgd
+
+
+def analyseFrame(frame, bkgd):
+    profile = get_profile(frame)
+    # cv2.imwrite(PATH + "scripts/output.jpg", roi)
+    profile -= bkgd
+    x = np.linspace(0, len(profile), len(profile))
+    # plt.plot(x, profile)
+    peak, props = find_peaks(profile, distance=len(profile), width=0)
+    width = 0
+    # print(f"props: {props}")
+    # print(f"peaks: {peak}\n")
+    if len(props['widths']) != 0:
+        width = props['widths'][0]
+    return width
+
+
 def getFrames():
-    cap = cv2.VideoCapture(PATH + "videos/vid08.mp4")
+    cap = cv2.VideoCapture(PATH + "videos/vid06.mp4")
     total_frames = int(cap.get(7))
 
+    bkgd = get_bkgd(cap, total_frames)
+
     widths = []
-    all = np.array([0.0]*586)
-    count = 0
 
     for i in range(1, total_frames):
         if i % 50 == 0:
             cap.set(1, i)
             ret, frame = cap.read()
-            width, profile = analyseFrame(frame)
+            width = analyseFrame(frame, bkgd)
             widths.append(width)
-            all += np.array(profile)
-            count += 1
-
-    all /= count
-    x = np.linspace(0, 600, len(all))
-    plt.plot(x, all)
-    plt.ylim(0, 200)
     
-    # x = np.linspace(0, len(widths), len(widths))
+    x = np.linspace(0, len(widths), len(widths))
 
-    # plt.plot(x, widths)
+    plt.plot(x, widths)
     plt.show()
-
-
-def analyseFrame(frame):
-    # print(frame.shape)
-    greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    roi = greyscale[114:700, 690:810]   # height, width
-    # cv2.imwrite(PATH + "scripts/output.jpg", roi)
-    profile = np.average(list(roi), 1)
-    x = np.linspace(0, len(profile), len(profile))
-    # plt.plot(x, profile)
-    peak, props = find_peaks(profile, prominence=20, width=0)
-    width = 0
-    # print(f"props: {props}")
-    # print(f"peaks: {peak}\n")
-    # if len(props['widths']) != 0:
-        # width = props['widths'][0]
-    return width, profile
-
 
 getFrames()
