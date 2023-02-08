8cbbb4df7c156a3ea48bb6686080f2a8237f2074
roddydr123 <davidroddy321@gmail.com>
2023-02-08 15:07:46 +0000

    light plotter

--- multiplot.py
+++ multiplot.py
@@ -16,13 +16,18 @@ def plotter(data, title, details_list):
         ind = np.argsort(dataset[0])
         x = np.array(dataset[0])[ind]
         y = np.array(dataset[1])[ind]
+        h = np.array(dataset[2])[ind]
         # smooth the line and plot it.
         ax.plot(x, gf1d(list(map(lambda x: x * 10, y)), 4), label=details_list[i])
+        newax = ax.twinx()
+        newax.plot(x, h, "r")
     ax.set_xlabel("Depth/cm")
     ax.set_ylabel("Slice thickness/mm")
     ax.legend()
     xlims = ax.get_xlim()
     ax.set_xlim(0, xlims[1])
+    ax.set_ylim(0, ax.get_ylim()[1])
+    newax.set_ylim(0, newax.get_ylim()[1])
     ax.yaxis.get_ticklocs(minor=True)
     ax.minorticks_on()
     plt.show()
