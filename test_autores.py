e93253aabc47f14d741c9f1ccb7ee62c798417c4
roddydr123 <davidroddy321@gmail.com>
2023-03-10 12:12:03 +0000

    wilcoxon test and more data

--- test_autores.py
+++ test_autores.py
@@ -33,9 +33,15 @@ all_diameters = np.array([s_diameters,
                             s_diameters,
                             f_diameters,
                             f_diameters,
-                            f_diameters])
+                            f_diameters,
+                            s_diameters,
+                            s_diameters,
+                            s_diameters,
+                            s_diameters,
+                            s_diameters,
+                            s_diameters])
 
-lengths = np.array([[0.0,2.0,10.8,22.7,39.6,44.9,57.0,65.8,69.3,72.5,75.8,76.8],
+lengths = np.array([[0.0,2.0,10.8,22.7,39.6,44.9,57.0,65.8,69.3,72.5,75.8,76.8],    # from EPP spreadsheet on teams
                     [0,2.4,26.5,39.4,52.4,71.3,83.1,85.4,85.5,85,86.2,97],
                     [0,2.2,27.3,42.1,53.9,72.6,85.6,84.5,86.5,87.5,89.2,96],
                     [0,2.3,12.2,19.5,29.3,44,57.7,64.5,68.2,69.8,71.3,72.5],
@@ -48,9 +54,15 @@ lengths = np.array([[0.0,2.0,10.8,22.7,39.6,44.9,57.0,65.8,69.3,72.5,75.8,76.8],
                     [0,0,3.8,18.1,23.9,70.1,85.1,128.6,147,165.6,165.1,192],
                     [0,0,10,31.2,46.4,71.2,85.4,91.8,134,139.1,139.1,139.1],
                     [0,4.4,14.3,22.1,31.9,41.2,55.9,63.1,67.2,71.7,72.7,73],
-                    [0,0,0,0,0,4.6,47.1,117.2,175.6,195,200.5,215.8],
+                    [0,0,0,0,0,4.6,47.1,117.2,175.6,195,200.5,215.8],               # from spreadsheet / NHS folder
                     [0, 0, 0, 0, 0, 22.9, 72.6, 108, 148, 197, 201, 233],
-                    [0.0, 0.0, 0.0, 0.0, 0.0, 41.0, 73.6, 121.0, 169.5, 197.1, 203.1, 221.5]])
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 41.0, 73.6, 121.0, 169.5, 197.1, 203.1, 221.5],
+                    [0.0,0.0,12.9,29.7,44.3,62.1,78.9,87.1,113.0,123.0,128.0,139.0],# from copy of pipe phantom data sheet on teams
+                    [0.0,0.0,0.0,0.0,0.0,22.9,72.6,108.0,148.0,197.0,201.0,233.0],  #
+                    [0.0,1.2,14.9,32.0,36.5,55.4,58.7,58.7,58.7,58.7,58.7,58.7],    #
+                    [0.0,0.0,19.6,31.7,36.8,53.4,53.6,56.0,58.5,58.5,58.5,58.5],    #
+                    [0.0,0.0,0.0,0.0,0.0,40.7,58.3,103.2,157.0,192.5,201.8,221.1],  #
+                    [0.0,0.0,0.0,0.0,0.0,38.1,62.1,123.7,149.3,199.2,204.2,225.8]]) #
 
 NHS_results = np.array([[70,52,0.75],
                         [102,68,0.66],
@@ -67,12 +79,19 @@ NHS_results = np.array([[70,52,0.75],
                         [68,48,0.71],
                         [70,179,2.56],
                         [73.5, 167, 2.27],
-                        [83,163,1.97]])
+                        [83,163,1.97],
+                        [95,81,0.85],
+                        [74,167,2.27],
+                        [72,48,0.7],
+                        [72,46,0.64],
+                        [77,159,2.1],
+                        [79,164,2.08]])
     
 
 EPP_ = []
 ST_ = []
 percent_difference = []
+difference = []
 
 num1 = 2
 num2 = 0
@@ -82,6 +101,7 @@ for diameters, lens, results in zip(all_diameters, lengths, NHS_results):
     EPP_.append(results[num1])
     ST_.append(ST_results[num2])
     percent_difference.append(abs(results[num1] - ST_results[num2]) * 100 / results[num1])
+    difference.append(results[num1] - ST_results[num2])
 
 result = linregress(EPP_, ST_)
 print(result)
@@ -95,3 +115,19 @@ plt.scatter(EPP_, ST_)
 plt.xlabel("EPP")
 plt.ylabel("ST")
 plt.show()
+
+# difference = np.array(difference)
+# args = np.argsort(abs(difference))
+# difference = difference[args]
+
+# ranks = list(zip(range(len(difference)), difference))
+
+# pos = [i[0] for i in ranks if i[1] > 0]
+# neg = [i[0] for i in ranks if i[1] < 0]
+
+# crit_val = min(sum(pos), sum(neg))
+
+# print(crit_val)
+# # print(ranks)
+# plt.hist(difference)
+# plt.show()
