5b44a671523f15e0ad1ea2dfbc3ab9d3a5892c96
roddydr123 <davidroddy321@gmail.com>
2023-03-25 14:41:35 +0000

    final corr plot

--- correlations.py
+++ correlations.py
@@ -4,37 +4,41 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
 
-
-# data = pd.read_csv("analysed/gen3/all_data.txt").drop(["Probe","R upper", "R lower", "Dr upper", "Dr lower", "Lr upper", "Lr lower"], axis=1)
-
-# plt.figure(figsize=(10,8))
-# sns.set_theme(style="white")
-# corr = data.corr()
-# heatmap = sns.heatmap(corr, annot=True, cmap="Blues", fmt='.2g')
-# plt.show()
-
-D1 = np.linspace(40,150, 10)
-D2 = 1.1 * D1 #+ (np.random.rand(10) * 1)
-
-# D2[0] *= 1.3
-# D2[-1] *= 0.87
-
-F1 = np.linspace(1,5,10)
-F2 = 0.5 * F1 + (np.random.rand(10) * 3)
-
-# G1 = np.random.rand(10) * 100
-# G2 = 4 * G1 + 10
-
-print(pearsonr(D1, D2))
-print(pearsonr(D2, F2))
-print(pearsonr(F1, F2))
-print(pearsonr(D1/F1, D2/F2))
-
-# print(D1, F1)
-# print(D1/F1)
+frequencies = [5, 9.5, 9.5, 6.5, 6.5, 3.5, 11.75, 11.75, 2.75, 3.75]
 
 
-plt.scatter(D1, D2)
-plt.scatter(F1,F2)
-plt.scatter(D1/F1, D2/F2)
+data = pd.read_csv("analysed/gen3/all_data.txt").drop(["Probe","R upper", "R lower", "Dr upper", "Dr lower", "Lr upper", "Lr lower"], axis=1)
+
+data["Frequency"] = frequencies
+
+plt.figure(figsize=(7,5))
+sns.set_theme(style="white")
+corr = data.corr()
+heatmap = sns.heatmap(corr, annot=True, cmap="Blues", fmt='.2g')
 plt.show()
+
+# D1 = np.linspace(40,150, 10)
+# D2 = 1.1 * D1 #+ (np.random.rand(10) * 1)
+
+# # D2[0] *= 1.3
+# # D2[-1] *= 0.87
+
+# F1 = np.linspace(1,5,10)
+# F2 = 0.5 * F1 + (np.random.rand(10) * 3)
+
+# # G1 = np.random.rand(10) * 100
+# # G2 = 4 * G1 + 10
+
+# print(pearsonr(D1, D2))
+# print(pearsonr(D2, F2))
+# print(pearsonr(F1, F2))
+# print(pearsonr(D1/F1, D2/F2))
+
+# # print(D1, F1)
+# # print(D1/F1)
+
+
+# plt.scatter(D1, D2)
+# plt.scatter(F1,F2)
+# plt.scatter(D1/F1, D2/F2)
+# plt.show()
